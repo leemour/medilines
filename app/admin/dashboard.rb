8b2948db9a54506983519f4c9669be4b8cce53fd
leemour <leemour@gmail.com>
2013-05-31 10:26:32 +0400

    Polished Admin Dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,25 +1,35 @@
 # encoding: UTF-8
+
 ActiveAdmin.register_page "Dashboard" do
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
     div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+      div :class => "blank_slate" do
+        #span I18n.t("active_admin.dashboard_welcome.welcome")
+        #small I18n.t("active_admin.dashboard_welcome.call_to_action")
+        span "Добро пожаловать в Панель Управления компании МедиЛайн Сервис."
+        small "Выберите раздел меню наверху, чтобы просмотреть/изменить данные."
       end
     end
 
-    panel "Недавно добавленные товары" do
+    panel "Недавно добавленные товары", :class => "recent-products" do
       table_for Product.recent(5).map do
         column 'Название', :name do |product|
           link_to(product.name, admin_product_path(product))
         end
+        column 'Бренд', :brand
+        column 'Категория', :category
+        column 'Фото', :image do |product|
+          image_tag(product.image.url(:thumb)) if product.image.url(:thumb)
+        end
         column 'Добавлен', :created_at
       end
-      strong { link_to "View All Products", admin_products_path }
+      strong { link_to "Все товары", admin_products_path }
     end
+
+
 
     # Here is an example of a simple dashboard with columns and panels.
     #
@@ -41,4 +51,9 @@ ActiveAdmin.register_page "Dashboard" do
     #   end
     # end
   end # content
+
+
+  sidebar "Помощь", :class => "help" do
+    "Нужна помощь? Звоните Славе или пишите на leemour@gmail.com"
+  end
 end
