b4c7dfa8e91f6b70616ec903fa51f4bfccfc10c5
leemour <leemour@gmail.com>
2013-06-13 11:23:26 +0400

    Completed main Menu highlighting on all levels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  # Generating page title if title exists
   def page_title(page)
     if page.title
       page.title + ' - '
@@ -7,7 +8,14 @@ module ApplicationHelper
     end
   end
 
+  # Defining root category for link_to url_for
   def root_cat(cat)
     cat.split('-')[0]
+  end
+
+  # Defining active menu class
+  def is_active?(slug)
+    slug.gsub!(/\//, '')
+    "active" if params[:page] == slug || params[:category] == slug
   end
 end
