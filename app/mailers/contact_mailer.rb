7c4daa11cd0ca514013d25811cebcde24d9aac24
leemour <leemour@gmail.com>
2013-06-14 16:28:14 +0400

    Fixed ContactMailer for Heroku

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 class ContactMailer < ActionMailer::Base
-  default to:   CONTACT_MAIL_TO
+  default to:   ENV['BUNDLE_GEMFILE']
 
   def contact_message(message)
     mail(from: "#{message.name} <#{message.email}>",
